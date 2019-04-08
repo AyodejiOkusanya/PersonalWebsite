ca95094572159770a228efffa4781284a191a3e7
github username <github email address>
2019-04-08 21:55:10 +0100

    added main project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,6 +19,20 @@ class Projects extends React.Component {
         <Grid.Row>
           <Grid.Column>
             <Image
+              href='https://youtu.be/kKOeFXIQKnE'
+              target='_blank'
+              src={require('../images/dex.png')}
+            />
+            <p style={{ paddingTop: '10px' }}>
+              An app that allows the user to build a website and write code
+              using only voice commeands.
+            </p>
+            <a href='https://youtu.be/kKOeFXIQKnE' target='_blank'>
+              Watch the demo!
+            </a>
+          </Grid.Column>
+          <Grid.Column>
+            <Image
               href='http://movie-hub.surge.sh/'
               target='_blank'
               src={require('../images/movieapp.png')}
@@ -28,7 +42,9 @@ class Projects extends React.Component {
               Rails. Includes authentication and database relationships for
               adding frineds and seeing their liked movies.
             </p>
-            <a href='http://movie-hub.surge.sh/' target='_blank'>Try it out!</a>
+            <a href='http://movie-hub.surge.sh/' target='_blank'>
+              Try it out!
+            </a>
           </Grid.Column>
           <Grid.Column>
             <Image
@@ -40,10 +56,12 @@ class Projects extends React.Component {
               A modern spin on the classic pong game. Build with Javascript,
               HTML Canvas and Ruby on Rails.
             </p>
-            <a href='http://pongplus.surge.sh/' target='_blank'>Try it out!</a>
+            <a href='http://pongplus.surge.sh/' target='_blank'>
+              Try it out!
+            </a>
           </Grid.Column>
           <Grid.Column>
-            <Image src={require('../images/foodblogapp.png')}  />
+            <Image src={require('../images/foodblogapp.png')} />
             <p style={{ paddingTop: '10px' }}>
               A food blogging website. Functionality includes posting articles,
               commenting on posts and sending private messages. Built with Ruby
